a63270acaca7d63f3000b0352214dfc0e4582c57
Ben Wagner <benjamin.j.wagner.1@gmail.com>
2023-05-06 18:34:55 -0400

    Minor fixes for major release; Optimized list functions; Updated version; Updated README

--- custom_components/custom_filters/__init__.py
+++ custom_components/custom_filters/__init__.py
@@ -16,9 +16,7 @@ _LOGGER = logging.getLogger(__name__)
 _TemplateEnvironment = template.TemplateEnvironment
 
 
-# --
-# - REPLACE ALL
-# --
+## -- REPLACE ALL
 def replace_all(text, find, replace = ''):
     """Replace all provided values with replacement value(s)"""
     find_all = find if isinstance(find, (list)) else [find]
@@ -28,9 +26,7 @@ def replace_all(text, find, replace = ''):
     return text
 
 
-# --
-# - IS DEFINED
-# --
+## -- IS DEFINED
 def is_defined(varname):
     """Check if a variable is defined by it's string name"""
     try:
@@ -41,17 +37,13 @@ def is_defined(varname):
         return True
 
 
-# --
-# - GET TYPE
-# --
+## -- GET TYPE
 def get_type(val):
     """Return the object type as a string"""
     return type(val).__name__
 
 
-# --
-# - IS TYPE
-# --
+## -- IS TYPE
 def is_type(val, typeof):
     """Check if a value is of given type"""
     if str(typeof) == typeof:
@@ -84,73 +76,62 @@ def is_type(val, typeof):
     return passchk
 
 
-# --
-# - INFLATE
-# --
+## -- INFLATE
 def inflate(string):
     """Inflates/compresses a string"""
     return zlib.compress(string.encode("utf-8"))
 
 
-# --
-# - DEFLATE
-# --
+## -- DEFLATE
 def deflate(string):
     """Deflates/decompresses a string"""
     return zlib.decompress(string)
 
 
-# --
-# - DECODE BASE64 AND INFLATE
-# --
+## -- DECODE BASE64 AND INFLATE
 def decode_base64_and_inflate(string):
     """Decodes and inflates a string"""
     data = base64.b64decode(string)
     return zlib.decompress(data).decode("utf-8")
 
 
-# --
-# - DEFLATE AND BASE64 ENCODE
-# --
+## -- DEFLATE AND BASE64 ENCODE
 def deflate_and_base64_encode(string):
     """Deflates and encodes a string"""
     data = zlib.compress(string.encode("utf-8"))
     return base64.b64encode(data).decode("utf-8")
 
 
-# --
-# - DECODE VALETUDO MAP
-# --
+## -- DECODE VALETUDO MAP
 def decode_valetudo_map(string):
     """Currently equivalent to deflate_and_base64_encode."""
     return decode_base64_and_inflate(string)
 
 
-# --
-# - UNQUOTE
-# --
-def unquote(string):
+## -- URL DECODE
+def urldecode(string):
     """Remove quotes from a string"""
     return urllib.parse.unquote(string)
 
 
-# --
-# - STRTOLIST
-# --
+## -- STRTOLIST
 def strtolist(string, delim=","):
     """Convert a string to a list"""
-    obj_res = re.sub(r"([\s]?)+['\"]+([\s]?)", "",
-                     string.strip((r"[]"))).strip()
+    obj_res = re.sub(r"([\s]?)+(['\"])+([\s]?)", "\\2", string.strip((r"[]"))).strip()
     if len(obj_res) == 0:
         obj_res = []
     else:
-        obj_res = obj_res.split(delim)
+        if delim != ",":
+            obj_res = obj_res.replace(delim, ",")
+        obj_res = "[" + obj_res.strip("[]") + "]"
+        try:
+            obj_res = ast.literal_eval(obj_res)
+        except ValueError:
+            obj_res = obj_res.split(",")
     return obj_res
 
 
-# --
-# - LISTIFY
-# --
+## -- LISTIFY
 def listify(string, delim=","):
     """Convert a string or non-list/dict into a list/dict"""
     if isinstance(string, (list, dict)):
@@ -161,8 +142,10 @@ def listify(string, delim=","):
         if obj_res.startswith('{') and obj_res.endswith('}'):
             obj_res = ast.literal_eval(obj_res)
         else:
-            if not obj_res.startswith('[') and not obj_res.endswith(']'):
-                obj_res = "[" + obj_res + "]"
+            if not obj_res.startswith('['):
+                obj_res = "[" + obj_res
+            if not obj_res.endswith(']'):
+                obj_res = obj_res + "]"
             # Convert to list or return the dict
             if obj_res.startswith('[') and obj_res.endswith(']'):
                 if obj_res == "[]":
@@ -173,9 +156,7 @@ def listify(string, delim=","):
     return obj_res
 
 
-# --
-# - GET INDEX
-# --
+## -- GET INDEX
 def get_index(obj, key, fallback=False):
     """Return the numeric index of a list or dict item"""
     # Normalize the list
@@ -193,9 +174,7 @@ def get_index(obj, key, fallback=False):
     return index_value
 
 
-# --
-# - GRAB
-# --
+## -- GRAB
 def grab(obj, key=0, fallback=""):
     """Get a list/dict item by key, with optional fallback"""
     # Normalize the object
@@ -223,9 +202,7 @@ def grab(obj, key=0, fallback=""):
     return my_val
 
 
-# --
-# - REACH
-# --
+## -- REACH
 def reach(obj, keypath, fallback=""):
     """Get a dict item by full path of key(s), with optional fallback"""
     res = {"found": True, "level": obj, "val": False}
@@ -245,9 +222,7 @@ def reach(obj, keypath, fallback=""):
     return res["level"]
 
 
-# --
-# - TERNARY
-# --
+## -- TERNARY
 def ternary(value, true_val, false_val, none_val=None):
     """Ternary evaluation fo True, False, or None values"""
     # value ? true_val : false_val
@@ -260,134 +235,124 @@ def ternary(value, true_val, false_val, none_val=None):
     return res
 
 
-# --
-# - RANDOMIZE LIST
-# --
-def randomize_list(mylist, seed=None):
+## -- RANDOMIZE/SHUFFLE LIST
+def shuffle(mylist):
     """Shuffle list"""
     try:
         mylist = listify(mylist)
-        if seed:
-            rand = Random(seed)
-            rand.shuffle(mylist)
-        else:
-            shuffle(mylist)
+        rand = Random()
+        rand.shuffle(mylist)
     except Exception:
         pass
     return mylist
 
 
-# --
-# - TO ASCII JSON
-# --
+## -- TO ASCII JSON
 def to_ascii_json(string):
     """Convert string to ASCII JSON"""
     return json.dumps(string, ensure_ascii=False)
 
 
-# --
-# - INIT
-# --
+
 def init(*args):
     """Initialize filters"""
     env = _TemplateEnvironment(*args)
-    env.filters["is_defined"] = is_defined
     env.filters["replace_all"] = replace_all
+    env.filters["is_defined"] = is_defined
     env.filters["get_type"] = get_type
     env.filters["is_type"] = is_type
-    env.filters["unquote"] = unquote
+    env.filters["inflate"] = inflate
+    env.filters["deflate"] = deflate
+    env.filters["decode_base64_and_inflate"] = decode_base64_and_inflate
+    env.filters["deflate_and_base64_encode"] = deflate_and_base64_encode
+    env.filters["decode_valetudo_map"] = decode_valetudo_map
+    env.filters["urldecode"] = urldecode
     env.filters["strtolist"] = strtolist
     env.filters["listify"] = listify
     env.filters["get_index"] = get_index
     env.filters["grab"] = grab
     env.filters["reach"] = reach
-    env.filters["urldecode"] = unquote
     env.filters["ternary"] = ternary
-    env.filters["shuffle"] = randomize_list
-    env.filters["deflate"] = deflate
-    env.filters["inflate"] = inflate
-    env.filters["deflate_and_base64_encode"] = deflate_and_base64_encode
-    env.filters["decode_base64_and_inflate"] = decode_base64_and_inflate
+    env.filters["shuffle"] = shuffle
     env.filters["to_ascii_json"] = to_ascii_json
-    env.filters["decode_valetudo_map"] = decode_valetudo_map
-    env.globals["is_defined"] = is_defined
     env.globals["replace_all"] = replace_all
+    env.globals["is_defined"] = is_defined
     env.globals["get_type"] = get_type
     env.globals["is_type"] = is_type
-    env.globals["unquote"] = unquote
-    env.globals["urldecode"] = unquote
+    env.globals["inflate"] = inflate
+    env.globals["deflate"] = deflate
+    env.globals["decode_base64_and_inflate"] = decode_base64_and_inflate
+    env.globals["deflate_and_base64_encode"] = deflate_and_base64_encode
+    env.globals["decode_valetudo_map"] = decode_valetudo_map
+    env.globals["urldecode"] = urldecode
     env.globals["strtolist"] = strtolist
     env.globals["listify"] = listify
     env.globals["get_index"] = get_index
     env.globals["grab"] = grab
     env.globals["reach"] = reach
     env.globals["ternary"] = ternary
-    env.globals["deflate"] = deflate
-    env.globals["inflate"] = inflate
-    env.globals["deflate_and_base64_encode"] = deflate_and_base64_encode
-    env.globals["decode_base64_and_inflate"] = decode_base64_and_inflate
+    env.globals["shuffle"] = shuffle
     env.globals["to_ascii_json"] = to_ascii_json
-    env.globals["decode_valetudo_map"] = decode_valetudo_map
     return env
 
 
 template.TemplateEnvironment = init
-template._NO_HASS_ENV.filters["is_defined"] = is_defined
 template._NO_HASS_ENV.filters["replace_all"] = replace_all
+template._NO_HASS_ENV.filters["is_defined"] = is_defined
 template._NO_HASS_ENV.filters["get_type"] = get_type
 template._NO_HASS_ENV.filters["is_type"] = is_type
-template._NO_HASS_ENV.filters["unquote"] = unquote
+template._NO_HASS_ENV.filters["inflate"] = inflate
+template._NO_HASS_ENV.filters["deflate"] = deflate
+template._NO_HASS_ENV.filters["decode_base64_and_inflate"] = decode_base64_and_inflate
+template._NO_HASS_ENV.filters["deflate_and_base64_encode"] = deflate_and_base64_encode
+template._NO_HASS_ENV.filters["decode_valetudo_map"] = decode_valetudo_map
+template._NO_HASS_ENV.filters["urldecode"] = urldecode
 template._NO_HASS_ENV.filters["strtolist"] = strtolist
 template._NO_HASS_ENV.filters["listify"] = listify
 template._NO_HASS_ENV.filters["get_index"] = get_index
 template._NO_HASS_ENV.filters["grab"] = grab
 template._NO_HASS_ENV.filters["reach"] = reach
-template._NO_HASS_ENV.filters["urldecode"] = unquote
 template._NO_HASS_ENV.filters["ternary"] = ternary
-template._NO_HASS_ENV.filters["shuffle"] = randomize_list
-template._NO_HASS_ENV.filters["deflate"] = deflate
-template._NO_HASS_ENV.filters["inflate"] = inflate
-template._NO_HASS_ENV.filters["deflate_and_base64_encode"] = deflate_and_base64_encode
-template._NO_HASS_ENV.filters["decode_base64_and_inflate"] = decode_base64_and_inflate
+template._NO_HASS_ENV.filters["shuffle"] = shuffle
 template._NO_HASS_ENV.filters["to_ascii_json"] = to_ascii_json
-template._NO_HASS_ENV.filters["decode_valetudo_map"] = decode_valetudo_map
-template._NO_HASS_ENV.globals["is_defined"] = is_defined
 template._NO_HASS_ENV.globals["replace_all"] = replace_all
+template._NO_HASS_ENV.globals["is_defined"] = is_defined
 template._NO_HASS_ENV.globals["get_type"] = get_type
 template._NO_HASS_ENV.globals["is_type"] = is_type
-template._NO_HASS_ENV.globals["unquote"] = unquote
-template._NO_HASS_ENV.globals["urldecode"] = unquote
+template._NO_HASS_ENV.globals["inflate"] = inflate
+template._NO_HASS_ENV.globals["deflate"] = deflate
+template._NO_HASS_ENV.globals["decode_base64_and_inflate"] = decode_base64_and_inflate
+template._NO_HASS_ENV.globals["deflate_and_base64_encode"] = deflate_and_base64_encode
+template._NO_HASS_ENV.globals["decode_valetudo_map"] = decode_valetudo_map
+template._NO_HASS_ENV.globals["urldecode"] = urldecode
 template._NO_HASS_ENV.globals["strtolist"] = strtolist
 template._NO_HASS_ENV.globals["listify"] = listify
 template._NO_HASS_ENV.globals["get_index"] = get_index
 template._NO_HASS_ENV.globals["grab"] = grab
 template._NO_HASS_ENV.globals["reach"] = reach
 template._NO_HASS_ENV.globals["ternary"] = ternary
-template._NO_HASS_ENV.globals["deflate"] = deflate
-template._NO_HASS_ENV.globals["inflate"] = inflate
-template._NO_HASS_ENV.globals["deflate_and_base64_encode"] = deflate_and_base64_encode
-template._NO_HASS_ENV.globals["decode_base64_and_inflate"] = decode_base64_and_inflate
+template._NO_HASS_ENV.globals["shuffle"] = shuffle
 template._NO_HASS_ENV.globals["to_ascii_json"] = to_ascii_json
-template._NO_HASS_ENV.globals["decode_valetudo_map"] = decode_valetudo_map
 
 
 async def async_setup(hass, hass_config):
     tpl = template.Template("", template._NO_HASS_ENV.hass)
-    tpl._env.globals = is_defined
     tpl._env.globals = replace_all
+    tpl._env.globals = is_defined
     tpl._env.globals = get_type
     tpl._env.globals = is_type
-    tpl._env.globals = unquote
+    tpl._env.globals = inflate
+    tpl._env.globals = deflate
+    tpl._env.globals = deflate_and_base64_encode
+    tpl._env.globals = decode_base64_and_inflate
+    tpl._env.globals = decode_valetudo_map
+    tpl._env.globals = urldecode
     tpl._env.globals = strtolist
     tpl._env.globals = listify
     tpl._env.globals = get_index
     tpl._env.globals = grab
     tpl._env.globals = reach
     tpl._env.globals = ternary
-    tpl._env.globals = deflate
-    tpl._env.globals = inflate
-    tpl._env.globals = deflate_and_base64_encode
-    tpl._env.globals = decode_base64_and_inflate
+    tpl._env.globals = shuffle
     tpl._env.globals = to_ascii_json
-    tpl._env.globals = decode_valetudo_map
     return True
